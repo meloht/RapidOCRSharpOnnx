759f52a79834253f5f352c5b05c71c36609e0760
luohongtao <luohongtao621@hotmail.com>
2026-05-29 14:28:47 +0800

    test

--- RapidOCRSharpOnnx.ConsoleGPU/Program.cs
+++ RapidOCRSharpOnnx.ConsoleGPU/Program.cs
@@ -7,7 +7,7 @@ namespace RapidOCRSharpOnnx.ConsoleGPU
 {
     internal class Program
     {
-        const int _deviceId = 0;
+        const int _deviceId = 1;
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
@@ -19,12 +19,12 @@ namespace RapidOCRSharpOnnx.ConsoleGPU
 
         private static void TestListSeq()
         {
-            string detectPath = @"C:\deeplearning\gitCode\meloht\RapidOCRSharpOnnx\RapidOCRSharpOnnx.Test\Models\ch_PP-OCRv5_det_mobile.onnx";
-            string recogPath = @"C:\deeplearning\gitCode\meloht\RapidOCRSharpOnnx\RapidOCRSharpOnnx.Test\Models\ch_PP-OCRv5_rec_mobile.onnx";
-            string clsPath = @"C:\deeplearning\gitCode\meloht\RapidOCRSharpOnnx\RapidOCRSharpOnnx.Test\Models\ch_PP-LCNet_x0_25_textline_ori_cls_mobile.onnx";
+            string detectPath = @"C:\deeplearning\gitCode\meloht\RapidOCRSharpOnnx\RapidOCRSharpOnnx.TestCommon\Models\ch_PP-OCRv5_det_mobile.onnx";
+            string recogPath = @"C:\deeplearning\gitCode\meloht\RapidOCRSharpOnnx\RapidOCRSharpOnnx.TestCommon\Models\ch_PP-OCRv5_rec_mobile.onnx";
+            string clsPath = @"C:\deeplearning\gitCode\meloht\RapidOCRSharpOnnx\RapidOCRSharpOnnx.TestCommon\Models\ch_PP-LCNet_x0_25_textline_ori_cls_mobile.onnx";
             //string saveDir = @"C:\code\model\OCRTestImagesResults";
             string saveDir = null;
-            using RapidOCRSharp ocr = new RapidOCRSharp(new ExecutionProviderCUDA(new OcrConfig(detectPath, recogPath, LangRec.CH, OCRVersion.PPOCRV5, clsPath), _deviceId));
+            using RapidOCRSharp ocr = new RapidOCRSharp(new ExecutionProviderDirectML(new OcrConfig(detectPath, recogPath, LangRec.CH, OCRVersion.PPOCRV5, clsPath), _deviceId));
             var list = Directory.GetFiles(@"C:\code\model\OCRTestImages");
             Stopwatch sw = new Stopwatch();
             sw.Start();
@@ -48,14 +48,14 @@ namespace RapidOCRSharpOnnx.ConsoleGPU
             //string detectPath = @"D:\code\RapidOCR-3.8.0\python\rapidocr\models\ch_PP-OCRv4_det_mobile.onnx";
             //string recogPath = @"D:\code\RapidOCR-3.8.0\python\rapidocr\models\ch_PP-OCRv4_rec_mobile.onnx";
             //string clsPath = @"D:\code\RapidOCR-3.8.0\python\rapidocr\models\ch_ppocr_mobile_v2.0_cls_mobile.onnx";
-          
+
             //string saveDir = null;
-            string detectPath = @"C:\deeplearning\gitCode\meloht\RapidOCRSharpOnnx\RapidOCRSharpOnnx.Test\Models\ch_PP-OCRv5_det_mobile.onnx";
-            string recogPath = @"C:\deeplearning\gitCode\meloht\RapidOCRSharpOnnx\RapidOCRSharpOnnx.Test\Models\ch_PP-OCRv5_rec_mobile.onnx";
-            string clsPath = @"C:\deeplearning\gitCode\meloht\RapidOCRSharpOnnx\RapidOCRSharpOnnx.Test\Models\ch_PP-LCNet_x0_25_textline_ori_cls_mobile.onnx";
+            string detectPath = @"C:\deeplearning\gitCode\meloht\RapidOCRSharpOnnx\RapidOCRSharpOnnx.TestCommon\Models\ch_PP-OCRv5_det_mobile.onnx";
+            string recogPath = @"C:\deeplearning\gitCode\meloht\RapidOCRSharpOnnx\RapidOCRSharpOnnx.TestCommon\Models\ch_PP-OCRv5_rec_mobile.onnx";
+            string clsPath = @"C:\deeplearning\gitCode\meloht\RapidOCRSharpOnnx\RapidOCRSharpOnnx.TestCommon\Models\ch_PP-LCNet_x0_25_textline_ori_cls_mobile.onnx";
             string saveDir = @"C:\code\model\OCRTestImagesResults";
 
-            using RapidOCRSharp ocr = new RapidOCRSharp(new ExecutionProviderCUDA(new OcrConfig(detectPath, recogPath, LangRec.CH, OCRVersion.PPOCRV5, clsPath), _deviceId));
+            using RapidOCRSharp ocr = new RapidOCRSharp(new ExecutionProviderDirectML(new OcrConfig(detectPath, recogPath, LangRec.CH, OCRVersion.PPOCRV5, clsPath), _deviceId));
             var list = Directory.GetFiles(@"C:\code\model\OCRTestImages");
             Stopwatch sw = new Stopwatch();
             sw.Start();
